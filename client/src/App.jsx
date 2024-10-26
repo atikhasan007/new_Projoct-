e80b316d7d04841deb2e98c0cd7006dfaa289d17
atik007 <atikhasan1971arik@gmail.com>
2024-10-26 15:25:55 +0600

    change 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function App() {
-  return <h1 className='text-red-500'>hello, world</h1>
+  return <h1 className='text-red-500'>hello, dgldkgjdalgadgdkgfldakgfdgkfdg</h1>
 }
