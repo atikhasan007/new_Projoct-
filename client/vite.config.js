2f014ca62f18cfa22582536f7d67abc82d842f2c
atik007 <atikhasan1971arik@gmail.com>
2024-10-31 18:06:17 +0600

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5010',
+        changeOrigin: true, // Ensures the origin header is modified to the target URL
+        secure: false,      // Disable SSL verification if the target is HTTPS
+      },
+    },
+  },
   plugins: [react()],
-})
+});
